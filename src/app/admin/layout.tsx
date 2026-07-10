512e7feea743ade511da9bc462e4cb3767eb48d8
shalom dahan <vdumpa972@gmail.com>
2026-07-10 09:08:24 -0700

    Add search metadata to public pages

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Administration | Rebar Planner",
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+    },
+  },
+};
+
+export default function PrivateRouteLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}
